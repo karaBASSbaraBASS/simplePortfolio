8e81b695b072df37fd96e06795814321fb2b5744
karaBASSbaraBASS <evgen.mikhalov@gmail.com>
2017-08-08 22:28:39 +0300

    section slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,15 @@ $(document).ready(function() {
             
             $(this).toggleClass('convert');
       });
-  
 
+    $(document).ready(function(){
+	  $('.slider1').bxSlider({
+	    slideWidth: 400,
+	    minSlides: 1,
+	    maxSlides: 3,
+	    moveSlides: 1,
+	    slideMargin: 10
+	  });
+	});
+  
 });
